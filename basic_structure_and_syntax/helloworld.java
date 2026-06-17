1821cd5d09e8e6bd4d11efe0b3d19fb28fd06627
vvlerie020 <valeriecamacho@gmail.com>
2026-06-17 13:02:52 +0800

    06/17/26 first commit

---
+++ basic_structure_and_syntax/helloworld.java
@@ -0,0 +1,9 @@
+package basic_structure_and_syntax; //this is a simplejava program that prints "Hello, World!" to the console. It consists of a class named "helloworld" and a main method that serves as the entry point of the program. The main method uses the System.out.println() function to output the string "Hello, World!" to the console when the program is executed.  
+
+
+public class helloworld { //this the class declaration, the name of the class is hello world
+    public static void main(String[] args) { //ito hahanapin ni terminal para umaandar ang program, ito yung main method na siyang entry point ng program
+        //yan ang front page
+        System.out.println("Hello, World!");
+    }
+}
