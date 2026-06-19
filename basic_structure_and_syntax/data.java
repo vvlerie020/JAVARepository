3cb7c85f608cb3d49d30636108dedb6eecb9a482
vvlerie020 <valeriecamacho@gmail.com>
2026-06-19 14:00:53 +0800

    activity 2 06/19/10

---
+++ basic_structure_and_syntax/data.java
@@ -0,0 +1,28 @@
+package basic_structure_and_syntax;
+
+public class data {
+    public static void main(String[] args) {
+        System.out.println("activity 2 Data Types");
+         byte myBite = 100; 
+         System.out.println("My mom gave me " + myBite + " pesos");
+        short myshort = 8000;
+        System.out.println("A regular phone might cost you " + myshort);
+        int myint = 100000;
+        System.out.println("An Ipad is ranging up to " + myint);
+        long mylong = 38098057091709L;
+        System.out.println("MY LRN is: " + mylong);
+        float myfloat = 17.5f;
+        System.out.println("The temperature of the aircobn is " + myfloat);
+        double mydouble = 0.6666666666666666;
+        System.out.println(" 60 / 90 = " + mydouble);
+        Boolean mine = true;
+         System.out.println("Dogs are furry. " + mine);
+        char mychar = 'A';
+        System.out.println("Your grade in English is: " + mychar);
+        char myVar1 = 123, myVar2 = 35, myVar3 = 789;
+        System.out.println("myVar1: " + myVar1);
+        System.out.println("You can put " + myVar2 + " in your facebook post");
+        System.out.println("The symbol when questioning is " + myVar3);
+        
+}
+}
